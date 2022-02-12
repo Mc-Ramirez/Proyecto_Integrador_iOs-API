c728619250aa3505f7a0542b59aa02ca4ed2c0c3
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-02-12 12:17:51 +0100

    Diseño de algunas pantallas

---
+++ Proyecto_Integrador_iOs+API/ViewControllerAddPlanet.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAddPlanet.swift
+//  Proyecto_Integrador_iOs+API
+//
+//  Created by Felipe Ramírez on 11/2/22.
+//
+
+import UIKit
+
+class ViewControllerAddPlanet: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
