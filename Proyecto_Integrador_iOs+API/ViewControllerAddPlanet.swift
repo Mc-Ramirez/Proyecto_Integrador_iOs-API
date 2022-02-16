1d45661aaace04bacabd06e145528c0cf783d4fa
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-02-16 18:32:53 +0100

    limpiando un poco el código

--- Proyecto_Integrador_iOs+API/ViewControllerAddPlanet.swift
+++ Proyecto_Integrador_iOs+API/ViewControllerAddPlanet.swift
@@ -1,66 +1,30 @@
-//
 //  ViewControllerAddPlanet.swift
 //  Proyecto_Integrador_iOs+API
-//
 //  Created by Felipe Ramírez on 11/2/22.
-//
 
 import UIKit
 
 class ViewControllerAddPlanet: UIViewController {
-    
     var decodePlanet : [Planetas] = []
     
-    @IBAction func btnAdd(_ sender: Any) {
-        executeAPI()
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
-        // Do any additional setup after loading the view.
     }
     
     func executeAPI(){
         let urlSession = URLSession.shared
         let url = URL(string: "https://iplanet-api.herokuapp.com/getallplanets")
-        
         urlSession.dataTask(with: url!) {
             data, response, error in
-           /* print("Data \(String(describing: data))")
-            print("Response \(String(describing: response))")*/
-            
             if let data = data {
                 let json = try? JSONSerialization.jsonObject(with: data)
                 print(String(describing: json)
             )}
             print("Error \(String(describing: error))")
         }.resume()
-        
-        /*func decodeJson(url : URL){
-            do{
-                let decoder = JSONDecoder()
-                let datosPlanetas = try Data(contentsOf: url)
-                
-                self.decodePlanet = try decoder.decode([Planetas].self, from: datosPlanetas)
-            } catch {
-                print("Error")
-            }
-        }*/
-        
-        
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func btnAdd(_ sender: Any) {
+        executeAPI()
     }
-    */
-
 }
